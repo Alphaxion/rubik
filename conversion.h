22a19c262b937cf843988a5d1f1d9554d680e498
sebMP <32484574+sebMP@users.noreply.github.com>
2017-10-17 15:55:02 +0200

    Add files via upload

---
+++ conversion.h
@@ -0,0 +1,9 @@
+#ifndef CONVERSION_H_INCLUDED
+#define CONVERSION_H_INCLUDED
+
+int* conversion (char* sol, int length);
+int* simplifyLevel2 (int* moves);
+int* simplifyLevel1(int* moves, int length);
+
+
+#endif // CONVERSION_H_INCLUDED
